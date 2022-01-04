81e7de2e0fe2706cb271eeb5e3a21aa4a7580835
miamollie <mia.deburca@99designs.com>
2022-01-04 22:11:58 +1100

    WIP - next try changing the gql lib

--- handler.go
+++ handler.go
@@ -14,71 +14,65 @@ import (
 // return value must be either error or TOut where TOut can marshalled
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("REQUEST URL: %#v", r.URL)
+	log.Printf("Query Params: %#v", r.URL.RawQuery) //needs to be decoded and then split to get the value
 	// Validate request: query params, auth header for slack, github token (?)
 
 	// make GQL request (will need to have some kinda private org repo auth check)
-	gqlresp := getCodeOwners("99designs", "aws-vault")
+	gqlresp := getCodeOwners("miamollie", "codeowners-slackbot")
 
 	// parse GQL response - and format
 
-	// write response
-
-	io.WriteString(w, gqlresp)
+	io.WriteString(w, gqlresp+" please fricking work")
 }
 
 // should receive a repo name and org, and return codeowners file content or nil (or empty string, or the actual message?), or an error
 func getCodeOwners(org string, repo string) string {
-	graphqlClient := graphql.NewClient("https://api.github.com/graphql") //TODO get from .env
+	graphqlClient := graphql.NewClient("https://api.github.com/graphql") //TODO get from .env. Maybe do the whole gql client setup as part of a service and pass in the endpoint?
 
-	// TODO change to
 	gqlQuery := `
-    query ($org: String!, $repo: String!)  {
-        repository(name: $repo", owner: $org) {
-			location1: object(expression: "master:readme.md") {
-				... on Blob {
-					text
-				}
-			}
-			location2: object(expression: "master:README.md") {
+    query ($repo: String!, $org: String!) {
+        repository(name: $repo, owner: $org) {
+    		location1: object(expression: "master:CODEOWNERS") {
 			... on Blob {
-					text
-				}
+				text
+			}
+    		location2: object(expression: "master:.gitignore/CODEOWNERS") {
+			... on Blob {
+				text
 			}
 		}
-    }
+	}
+}
 `
 	req := graphql.NewRequest(gqlQuery)
 
+	req.Header.Set("Cache-Control", "no-cache")
+
+	// this is the only bit that needs to happen per request
 	req.Var("org", org)
 	req.Var("repo", repo)
 
-	req.Header.Set("Cache-Control", "no-cache")
 
 	// run it and capture the response
 	var resp RepoResponse
 	if err := graphqlClient.Run(context.Background(), req, &resp); err != nil {
-		//TODO log the error
 		log.Printf("GQL error: %#v", err)
 
-		return "Error making request"
+		return "Error making request, soz"
 	}
 
-	// if resp.errors != nil {
-	// 	return resp.errors.message, nil
-	// }
 	log.Printf("GQL resp: %#v", resp)
 	// TODO split the making of this request and the formatting of the response
 	// TODO resp.errors.message might not be null
-	return "Made it here"
+	return resp.data.repository.location1.text
 
 }
 
-type RepoContents struct {
+type repoContents struct {
 	text string
 }
 
-type RepoResponse struct {
+type repoResponse struct {
 	data struct {
 		repository struct {
 			location1 RepoContents
